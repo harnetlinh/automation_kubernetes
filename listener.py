30291de1f667edb21d351a0b09371a9a3672d927
Ha Ngoc Linh <linhhn13@fe.edu.vn>
2022-12-10 16:08:48 +0700

    update listener

--- listener.py
+++ listener.py
@@ -9,16 +9,16 @@ session = init_aws_session()
 elb = session.client('elbv2')
 
 response = elb.describe_load_balancers()
-pprint.pprint(response['LoadBalancers']['AvailabilityZones'][0]['LoadBalancerArn'])
+print(response['LoadBalancers'][0]['LoadBalancerArn'])
 
 response = elb.create_listener(
     DefaultActions=[
         {
-            'TargetGroupArn': ['arn:aws:elasticloadbalancing:ap-northeast-1:048452126007:targetgroup/unbiased-coder-target-group/e3469cbd536318fb'],
+            'TargetGroupArn': 'arn:aws:elasticloadbalancing:ap-northeast-1:048452126007:targetgroup/unbiased-coder-target-group/e3469cbd536318fb',
             'Type': 'forward',
         },
     ],
-    LoadBalancerArn= response['LoadBalancers']['AvailabilityZones'][0]['LoadBalancerArn']
+    LoadBalancerArn=response['LoadBalancers'][0]['LoadBalancerArn'],
     Port=80,
     Protocol='HTTP'
 )
