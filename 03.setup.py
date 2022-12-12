386f5611a2e95602b2015867b1c72cd17fed3f44
Son N. Nguyen <a.son.n.nguyen@gmail.com>
2022-12-13 02:24:37 +0700

    clean code

--- 03.setup.py
+++ 03.setup.py
@@ -84,89 +84,7 @@ def setup_instance(instance_type, instance, join_command=None):
         print(stdout)
         print(stderr)
         return "ok"
-        
-def setup_instance_debug(instance_type, instance, join_command=None):
-    """setup master and slave instances
-
-    Args:
-        instance_type (string): "master" or "slave"
-        instance (object): instance object
-    """
-    pem_key_name = os.getenv('AWS_PEM_KEY') + '.pem'
-    # print("Connecting to " + instance_type + " instance")
-    # print('pem_key_name: ' + pem_key_name)
-    # print('public ip: ' + instance['PublicIpAddress'])
-    # print('ssh_username: ' + os.getenv('SSH_USERNAME'))
-    ssh_username = os.getenv('SSH_USERNAME')
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.load_system_host_keys()
-    client.connect(hostname=instance['PublicIpAddress'],
-                   username=ssh_username, key_filename=pem_key_name)
-    # pprint("Connected to " + instance_type + " instance")
-    """
-    stdin, stdout, stderr = client.exec_command('sudo apt install git -y')
-    test = stdout.readlines()
-    print('sudo apt install git -y')
-
-    stdin, stdout, stderr = client.exec_command(
-        'rm -rf automation_kubernetes_test')
-    print(stdout.readlines())
-    stdin, stdout, stderr = client.exec_command(
-        'git clone https://github.com/harnetlinh/automation_kubernetes_test.git')
-    # time.sleep(50);
-    print(stdout.readlines())
-    print(stderr.readlines())
-    stdin, stdout, stderr = client.exec_command('cd automation_kubernetes_test/')
-    print(stdout.readlines())
-    print(stderr.readlines())
-    stdin, stdout, stderr = client.exec_command('ls')
-
-    print(stdout.readlines())
-    stdin, stdout, stderr = client.exec_command('sudo bash')
-    """
-    if instance_type == 'master':
-        """
-        stdin, stdout, stderr = client.exec_command(
-            'chmod +x /home/ubuntu/automation_kubernetes_test/01.master01.sh')
-        print('chmod +x /home/ubuntu/automation_kubernetes_test/01.master01.sh')
-        print(stdout.readlines())
-        
-        stdin, stdout, stderr = client.exec_command(
-            'chmod +x /home/ubuntu/automation_kubernetes_test/01.master02.sh')
-        print('check folder')
-        print(stdout.readlines())
-        stdin, stdout, stderr = client.exec_command(
-            'sudo ~/automation_kubernetes_test/01.master01.sh')
-        print(stdout.readlines())
-        print(stderr.readlines())
-        """
-        join = get_join_command_from_master(client)
-        stdin, stdout, stderr = client.exec_command(
-            'sudo /home/ubuntu/automation_kubernetes_test/01.master02.sh')
-        print(stdout)
-        print(stderr)
-        
-        return join
-
-    else:
-        if join_command is None:
-            raise Exception('join command is None')
-            
-        stdin, stdout, stderr = client.exec_command(
-            'chmod +x /home/ubuntu/automation_kubernetes_test/02.slave.sh')
-        print(stdout)
-        print(stderr)
-        stdin, stdout, stderr = client.exec_command(
-            'sudo /home/ubuntu/automation_kubernetes_test/02.slave.sh')
-        
-        print(stderr)
-        stdin, stdout, stderr = client.exec_command(join_command)
-        print(stdout)
-        print(stderr)
-        
-        return "ok"
-
+  
 def get_join_command_from_master(client):
     """get join command from master instance"""
 
@@ -186,7 +104,7 @@ reservation = reservations[0]
 instances = reservation['Instances']
 master = instances[0]
 list_slave = instances[1:]
-join_command = setup_instance_debug('master', master)
+join_command = setup_instance('master', master)
 for slave in list_slave:
     print('setup slave')
     setup_instance('slave', slave, join_command)
